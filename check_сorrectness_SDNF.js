bf648671b7eb6f5777683ef755736d2ac37e232f
diana sazanchuk <dianasazanchuk@gmail.com>
2020-04-28 23:54:33 +0300

    works

--- check_сorrectness_SDNF.js
+++ check_сorrectness_SDNF.js
@@ -1,65 +1,147 @@
-var unaryOrBinaryComplexFormula = new RegExp('([(][!]([A-Z])[)])|([(]([A-Z])((&)|(\\|)|(->)|(~))([A-Z])[)])','g');
-var unaryOrBinaryComplexFormula1 = new RegExp('([(][!]([A-Z])[)])|([(]([A-Z])((&)|(\\|)|(->)|(~))([A-Z])[)])');
-var atomOrConstant = new RegExp('([A-Z]|[0-1])', 'g');
-var replaceFormula = "R";
-
-function checkCorrectnessSDNF() {
+function checking(){
     let formula = document.getElementById("probablySDNF").value;
-    let answer = document.getElementById("answer");
-
-    if (isFormula(formula)) {
-        answer.innerHTML = checkCorrectnessConjunctions(formula);  //Проверка корректности конъюнкций
-    }
-    else 
-        answer.innerHTML = "Не формула";
+    checkCorrectnessSDNF(formula);
 }
 
-function checkCorrectnessConjunctions(formula) {
-    //Разбиение исходной формулы на конъюнкции
-    let arrayOfDisjunctions = formula.split("|"); 
-    let arrayOfUsedVariables = [];
-    for (let i = 0; i < arrayOfDisjunctions.length; i++) {
-        for (let j = 0; j < arrayOfDisjunctions.length; j++) {
-            if (i != j) {  
-                //Определение используемых в конъюнкции переменных
-                arrayOfUsedVariables = arrayOfDisjunctions[i].match(/([A-Z]|(![A-Z]))+/g);
+function checkCorrectnessSDNF(formula) {
+    let answer = document.getElementById("answer");
+    let choice = document.getElementById("chooseIfSDNF").value;
+    let choiceAnswer = document.getElementById("choiceAnswer");
 
-                //Поиск одинаковых конъюнкций
-                let equals = 0;
-                for (let k = 0; k < arrayOfUsedVariables.length; k++) {
-                    if (arrayOfUsedVariables[k] == arrayOfDisjunctions[j].match(/([A-Z]|(![A-Z]))+/g)[k])
-                        equals++;
-                    else 
-                        equals--;
-                }
-                if (equals == arrayOfUsedVariables.length)
-                    return "Формула не является СДНФ. Найдены одинаковые конъюнкции " + arrayOfDisjunctions[i] + ".";
+    //Проверка наличия лишних символов
+    if (formula.match(/(?!([A-Z]|&|\||\(|\)|!))./g)) {  //(?!) - искать любые символы, кроме заключённых в данные скобки
+        answer.innerHTML = "Некорректные символы.";
+        return;
+    }
 
-                //Проверка каждой конъюнкции на использование всех переменных
-                for (let k = 0; k < arrayOfUsedVariables.length; k++) {
-                    if (arrayOfUsedVariables[k].length == 1)
-                        equals = arrayOfDisjunctions[j].indexOf(arrayOfUsedVariables[k]);
-                    else 
-                        equals = arrayOfDisjunctions[j].indexOf(arrayOfUsedVariables[k][1]);
-                    if (equals == -1)
-                        return "Формула не является СДНФ. Найдена конъюнкция " + arrayOfDisjunctions[j] + ", содержащая не все переменные.";
-                }
+    if (formula.match(/\([A-Z]\)/g)){
+        answer.innerHTML = "Введенная строка не является СДНФ";
+    }
+    else if (formula != "") {
+        if (formula.length == 1) {
+            let reg = /[A-Z]/g
+            if (reg.test(formula)) {
+                answer.innerHTML = "Формула является СДНФ.";
+            } else {
+                answer.innerHTML = "Введенная строка не является СДНФ";
+            }
+        } else {
+           if (checkWithRegularExpressionFormula(formula)) {  //Проверка соответствия грамматике
+                if (formula.match(/\)\|\(/g))  //Проверка наличия конъюнкций
+                    if (checkAllConjunctions(formula, '|')) {
+                        answer.innerHTML = "Формула является СДНФ.";
+                    } else {
+                        answer.innerHTML = "Формула не является СДНФ."
+                    }
+                else
+                    answer.innerHTML = "Конъюнкции не найдены.";
+            }
+            else
+                answer.innerHTML = "Формула не соответствует правилам грамматики.";
+        }
+    }
+
+    let isRight = (answer.innerHTML == "Формула является СДНФ.");
+
+    const a = (choice == 1 && isRight);
+    const b = (choice == 0 && !isRight);
+    if (a || b)  {
+        choiceAnswer.innerHTML = "Вы были правы";
+    } else {
+        choiceAnswer.innerHTML = "Вы не угадали";
+    }
+}
+
+
+function checkAllConjunctions(expression, sep) {
+    let conjunctionOperationsArray = expression.split(sep);
+    if (conjunctionOperationsArray == false) return false
+    let j, k;
+    let conj = '&';
+    let resBooltrue = false;
+    let resBoolfalse = false;
+    let eqResBool = false;
+    for (j = 0; j < conjunctionOperationsArray.length; j++) {
+        for (k = 0; k < conjunctionOperationsArray.length; k++) {
+            if (j != k) {
+                resBooltrue = compareConjunctions(conjunctionOperationsArray[j].split(conj), conjunctionOperationsArray[k].split(conj), true);
+                resBoolfalse = compareConjunctions(conjunctionOperationsArray[j].split(conj), conjunctionOperationsArray[k].split(conj), false);
+                if (resBooltrue == false || resBoolfalse == false) return false
+            } else {
+                eqResBool = compareSingleConjunction(conjunctionOperationsArray[j].split(conj));
+                if (eqResBool == false) return false
             }
         }
     }
-    return "Формула является СДНФ.";
+    return true
 }
 
-function isFormula(formula) { //проверка является ли строка формулой
-    let tempFormula
-    while (formula != tempFormula) {
-        tempFormula = formula;
-        formula = formula.replace(unaryOrBinaryComplexFormula, replaceFormula);
+
+function compareConjunctions(first, second, type) {
+    console.log(first, second, type)
+    let x, y;
+    let bool;
+    if (first.length != second.length) return false
+    for (x = 0; x < first.length; x++) {
+        bool = false;
+        for (y = 0; y < second.length; y++) {
+            if (type) {
+                first[x] = first[x].replace(/[!()]/g, '');
+                second[y] = second[y].replace(/[!()]/g, '');
+            } else {
+                first[x] = first[x].replace(/[()]/g, '');
+                second[y] = second[y].replace(/[()]/g, '');
+            }
+            if (first[x] == second[y]) {
+                bool = true;
+                break;
+            }
+        }
+        if (type) {
+            if (!bool) return false
+        } else {
+            if (bool == false) return true
+        }
     }
-    tempFormula = 0;
-    if ((formula.length == 1)) {
+    return type;
+}
+
+
+function compareSingleConjunction(first) {
+    let x, y;
+    let bool;
+    for (x = 0; x < first.length; x++) {
+        bool = true
+        for (y = 0; y < first.length; y++) {
+            if (x != y) {
+                first[x] = first[x].replace(/[!()]/g, '');
+                first[y] = first[y].replace(/[!()]/g, '');
+                if (first[x] == first[y]) return false;
+            }
+        }
+        if (bool != true) return false;
+    }
+    return bool;
+}
+
+function checkWithRegularExpressionFormula(formula) {
+    const FORMULA_REGEXP = new RegExp('([(]([A-Z]|[0-1])((->)|(&)|(\\|)|(~))([A-Z]|[0-1])[)])|([(][!]([A-Z]|[0-1])[)])|([A-Z])|([0-1])','g');
+    let form = formula;
+
+    if (form.length == 1 && form.match(/[A-Z]|[0-1]/)) {
         return true;
     } else {
-        return false;
+        while (true) {
+            let initLength = form.length;
+            form = form.replace(FORMULA_REGEXP, '1')
+            if (form.length === initLength) {
+                break;
+            }
+        }
+        if ((form.length === 1) && (form.match(/1/))) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
